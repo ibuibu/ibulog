8269bbc612f0d00ad9ad5319de02c8389742866c
ibuibu <ibuibu69@gmail.com>
2021-07-31 21:57:38 +0900

    Change webpack5 setting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
   },
 };
