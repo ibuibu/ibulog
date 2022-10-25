93eb55a772f4ba1e2422594c9e9c0a593b4b9f32
ibuibu <ibuibu69@gmail.com>
2022-10-25 22:34:31 +0900

    Add fontawesome icon

--- src/pages/experiments.tsx
+++ src/pages/experiments.tsx
@@ -1,3 +1,5 @@
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../components/layout";
 
 export default function Home() {
@@ -8,7 +10,13 @@ export default function Home() {
       <a href={path} target="_blank" rel="noopener noreferrer">
         <div className="flex p-4 my-2 w-full border border-black transition duration-200 transform hover:scale-105 hover:rotate-2 cursor-pointer">
           <div>
-            <p className="mb-2 text-2xl font-bold">{title}</p>
+            <p className="mb-2 text-2xl font-bold">
+              {title}
+              <FontAwesomeIcon
+                icon={faUpRightFromSquare}
+                className="mx-2 text-sm"
+              />
+            </p>
             <p>TypeScriptで実装した、Pure Data (Pd) のようなWebアプリ</p>
           </div>
         </div>
